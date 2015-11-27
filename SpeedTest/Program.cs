f88c78048d9ae2b639613eed344e874c396fc4b6
Islam-Mamayeu <Islam_Mamayeu@epam.com>
2015-11-27 17:07:47 +0300

    ArrayList and LinkedList realisation

--- SpeedTest/Program.cs
+++ SpeedTest/Program.cs
@@ -10,6 +10,43 @@ namespace SpeedTest
     {
         static void Main(string[] args)
         {
+            string choice = null;
+            bool isInt;
+            bool b = true;
+
+            while (b)
+            {
+
+                Console.WriteLine("1.Add");
+                Console.WriteLine("2.Delete");
+                Console.WriteLine("3.Search");
+                Console.WriteLine("4.exit\n");
+                choice = Console.ReadLine();
+
+
+                switch (choice)
+                {
+                    case "1":
+                        Util.Add();
+                        break;
+                    case "2":
+                        Util.Delete();
+                        break;
+                    case "3":
+                        Util.Delete();
+                        break;
+                    case "4":
+                        b = false;
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+
+
+
         }
     }
 }
--- SpeedTest/Util.cs
+++ SpeedTest/Util.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpeedTest
 {
-    class Util
+     class Util
     {
+        static public void Add()
+        {
+            ArrayList list = new ArrayList();
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i=0;i<1000000;i++)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine("List:" + stopwatch.Elapsed.Seconds+ ":" + stopwatch.Elapsed.Milliseconds);
+
+            stopwatch.Restart();
+            for(int i=0;i<1000000;i++)
+            {
+                linkedList.AddLast(i);
+            }
+
+            Console.WriteLine("List:" + stopwatch.Elapsed.Seconds + ":" + stopwatch.Elapsed.Milliseconds);
+
+        }
+        static public void Delete()
+        {
+            ArrayList list = new ArrayList();
+        }
+        static public void Search()
+        {
+            ArrayList list = new ArrayList();
+        }
+
+        
+
     }
 }
